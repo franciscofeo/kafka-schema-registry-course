e0740828cec9774cb69ec164d562d99da66a0b84
angelo <franciscoangelo.feo@gmail.com>
2022-07-23 21:14:39 -0300

    feat: Avro generic record builder in Java

---
+++ avro-in-java/src/main/java/br/com/franciscoangelo/GenericRecordExample.java
@@ -0,0 +1,84 @@
+package br.com.franciscoangelo;
+
+import java.io.File;
+import java.io.IOException;
+import org.apache.avro.Schema;
+import org.apache.avro.file.DataFileReader;
+import org.apache.avro.file.DataFileWriter;
+import org.apache.avro.generic.GenericData;
+import org.apache.avro.generic.GenericDatumReader;
+import org.apache.avro.generic.GenericDatumWriter;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.avro.io.DatumReader;
+import org.apache.avro.io.DatumWriter;
+
+public class GenericRecordExample {
+
+    public static void main(String[] args) {
+
+        // step 0: define your schema
+
+        Schema.Parser schemaParser = new Schema.Parser();
+        Schema customerSchema = schemaParser.parse("{\n" +
+                "     \"type\": \"record\",\n" +
+                "     \"namespace\": \"com.example\",\n" +
+                "     \"name\": \"Customer\",\n" +
+                "     \"fields\": [\n" +
+                "       { \"name\": \"first_name\", \"type\": \"string\", \"doc\": \"First Name of Customer\" },\n" +
+                "       { \"name\": \"last_name\", \"type\": \"string\", \"doc\": \"Last Name of Customer\" },\n" +
+                "       { \"name\": \"age\", \"type\": \"int\", \"doc\": \"Age at the time of registration\" },\n" +
+                "       { \"name\": \"height\", \"type\": \"float\", \"doc\": \"Height at the time of registration in m\" },\n" +
+                "       { \"name\": \"weight\", \"type\": \"float\", \"doc\": \"Weight at the time of registration in kg\" },\n" +
+                "       { \"name\": \"automated_email\", \"type\": \"boolean\", \"default\": true, \"doc\": \"Field indicating if the user is enrolled in marketing emails\" }\n" +
+                "     ]\n" +
+                "}");
+
+        // step 1: create a generic record
+
+        GenericRecordBuilder customerBuilder = new GenericRecordBuilder(customerSchema);
+        GenericData.Record customer = customerBuilder
+                .set("first_name", "Francisco")
+                .set("last_name", "Angelo")
+                .set("age", 22)
+                .set("height", 1.72F)
+                .set("weight", 82.5F)
+                .set("automated_email", false)
+                .build();
+
+        System.out.println(customer);
+
+        // step 3: write to a file
+        final DatumWriter<GenericRecord> datumWriter = new GenericDatumWriter<>(customerSchema);
+        try (DataFileWriter<GenericRecord> dataFileWriter = new DataFileWriter<>(datumWriter)) {
+            dataFileWriter.create(customer.getSchema(), new File("customer-generic.avro"));
+            dataFileWriter.append(customer);
+            System.out.println("Written customer-generic.avro");
+            dataFileWriter.close();
+        } catch (IOException e) {
+            System.out.println("Couldn't write file");
+            e.printStackTrace();
+        }
+
+        // step 4: read from a file
+        final File file = new File("customer-generic.avro");
+        final DatumReader<GenericRecord> datumReader = new GenericDatumReader<>();
+        GenericRecord customerRead;
+        try (DataFileReader<GenericRecord> dataFileReader = new DataFileReader<>(file, datumReader)){
+            customerRead = dataFileReader.next();
+            System.out.println("Successfully read avro file");
+            System.out.println(customerRead.toString());
+
+            // get the data from the generic record
+            System.out.println("First name: " + customerRead.get("first_name"));
+
+            // read a non existent field
+            System.out.println("Non existent field: " + customerRead.get("not_here"));
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
